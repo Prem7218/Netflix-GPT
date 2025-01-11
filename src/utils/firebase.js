d97931248a4fa603c72a04755da240262cadebd1
Prem7218 <premradheshyammahajan@gmail.com>
2025-01-11 11:23:28 +0530

    Firebase ID, API

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -8,7 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  
+    apiKey: "AIzaSyCJF_CwEa-sZFCmAPsMkmuXOB0n2zpqstc",
+    authDomain: "movie-flex-b5388.firebaseapp.com",
+    projectId: "movie-flex-b5388",
+    storageBucket: "movie-flex-b5388.firebasestorage.app",
+    messagingSenderId: "82756034235",
+    appId: "1:82756034235:web:c1e7031021aef44206d39e",
+    measurementId: "G-4MMQ8X1QCW"
 };
 
 // Initialize Firebase
