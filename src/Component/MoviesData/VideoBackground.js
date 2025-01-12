7216fd6b4e9af6c34575662a5d4b33732a4b5bf9
PREM RADHESHYAM MAHAJAN <161498035+Prem7218@users.noreply.github.com>
2025-01-12 14:15:17 +0530

    Update VideoBackground.js
    uncomment

--- src/Component/MoviesData/VideoBackground.js
+++ src/Component/MoviesData/VideoBackground.js
@@ -74,7 +74,7 @@ const VideoBackground = ({ id, onEnded }) => {
       className="w-screen aspect-video"
       src={`https://www.youtube.com/embed/${trailerKey}?autoplay=1&mute=1`}
       title="YouTube video player"
-      // referrerPolicy="strict-origin-when-cross-origin"
+      referrerPolicy="strict-origin-when-cross-origin"
       allow="autoplay; encrypted-media"   
     ></iframe>
   );
